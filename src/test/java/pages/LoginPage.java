1291dd672f18f64525dce361d97b0eb845dbe1d1
rekha831 <82863914+rekha831@users.noreply.github.com>
2025-10-07 23:32:01 +0530

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -5,77 +5,62 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class LoginPage extends BasePage {
 
+    private WebDriverWait wait;
 
-@FindBy(xpath = "//input[@data-testid='user-mobileno-input-box']")
-private WebElement txtUsername;
+    // Locators
+    private By txtUsernameLocator = By.id("username");
+    private By txtPasswordLocator = By.cssSelector("[type='password']");
+    private By btnLoginLocator = By.id("signInBtn");
+    private By loginButtonTextLocator = By.id("signInBtn");
+    private By dashboardTextLocator = By.xpath("//a[text()='ProtoCommerce Home']");
+    private By errorTextLocator = By.xpath("//div[text()='Please enter valid mobile number']");
 
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30)); // explicit wait
+    }
 
-@FindBy(xpath = "//input[@data-testid='password-input-box-cta']")
-private WebElement txtPassword;
+    // Enter Username
+    public void enterUsername(String username) {
+        WebElement mobileInput = wait.until(ExpectedConditions.visibilityOfElementLocated(txtUsernameLocator));
+        mobileInput.clear();
+        mobileInput.sendKeys(username);
+    }
 
+    // Enter Password
+    public void enterPassword(String password) {
+        WebElement passInput = wait.until(ExpectedConditions.visibilityOfElementLocated(txtPasswordLocator));
+        passInput.clear();
+        passInput.sendKeys(password);
+    }
 
-@FindBy(xpath = "//div[@data-testid='login-cta']")
-private WebElement btnLogin;
+    // Click Login Button
+    public void clickLogin() {
+        WebElement loginBtn = wait.until(ExpectedConditions.elementToBeClickable(btnLoginLocator));
+        loginBtn.click();
+    }
 
-@FindBy(xpath = "//div[text()='Login']")
-private WebElement LogiinButton1;
+    // Click "Login" button text (alternative)
+    public void clickLoginBtnText() {
+        WebElement loginBtnText = wait.until(ExpectedConditions.elementToBeClickable(loginButtonTextLocator));
+        loginBtnText.click();
+    }
 
-@FindBy(xpath = "//div[text()='Hi jagadeesh']")
-private WebElement dashboardText;
+    // Get Dashboard Text
+    public String getDashboardText() {
+        WebElement dashText = wait.until(ExpectedConditions.visibilityOfElementLocated(dashboardTextLocator));
+        return dashText.getText();
+    }
 
-@FindBy(xpath = "//div[text()='Please enter valid mobile number']")
-private WebElement errorText;
-
-
-public LoginPage(WebDriver driver) {
-super(driver);
-}
-
-/*
-public void enterUsername(String user) {
-//txtUsername.clear();
-txtUsername.sendKeys(user);
-}*/
-
-public void enterUsername(String user) {
-	//txtUsername.clear();
-	//txtUsername.sendKeys(user);
-	
-WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-WebElement mobileInput = wait.until(
-    ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@data-testid='user-mobileno-input-box']"))
-);
-mobileInput.sendKeys(user);
-
-}
-public void enterPassword(String pass) {
-//txtPassword.clear();
-txtPassword.sendKeys(pass);
-}
-
-
-public void clickLogin() {
-btnLogin.click();
-}
-
-public void clickLoginBtn() {
-	LogiinButton1.click();
-}
-
-public String getText() {
-	return dashboardText.getText();
-	
-}
-
-public String getloginErrorText() {
-	return errorText.getText();
-	
-}
+    // Get Login Error Text
+    public String getLoginErrorText() {
+        WebElement errText = wait.until(ExpectedConditions.visibilityOfElementLocated(errorTextLocator));
+        return errText.getText();
+    }
 }
