baee3465d61bece901d4862a73687c4ab25c0f9b
rekha831 <rekharam392@gmail.com>
2025-10-09 12:58:54 +0530

    adding the changes of allure

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,13 +3,23 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
 @CucumberOptions(
-	    features = "src/test/resources/features",
-	    glue = {"stepdefinitions", "hooks"},
-	    plugin = {"pretty", "html:target/cucumber-reports.html"},
-	    monochrome = true
-	)
+    features = "src/test/resources/features",
+    glue = {"stepdefinitions", "hooks"},
+    
+    // ✅ Add Allure plugin along with pretty & html
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports.html",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" // <-- Allure integration
+    },
+    
+    monochrome = true
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
-// if you need parallel execution of scenarios, override scenarios() and use DataProvider
+    // ✅ Optional: For parallel execution, you can override scenarios() with a DataProvider
+    // @DataProvider(parallel = true)
+    // public Object[][] scenarios() {
+    //     return super.scenarios();
+    // }
 }
